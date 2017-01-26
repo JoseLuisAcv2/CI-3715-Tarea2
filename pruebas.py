7c12120d9e6be9f537800d85222868e099cbd87f
Jose Acevedo <joseluisacevedo1995@gmail.com>
2017-01-26 09:30:12 -0400

    Agregar archivo para pruebas

---
+++ pruebas.py
@@ -0,0 +1,18 @@
+#
+#   Laboratorio de Ingenieria de Software - Tarea 2
+#
+#   Autores:    - Jose Acevedo      13-10006
+#               - Augusto Hidalgo   13-10665
+#
+
+import unittest
+from calcularPrecio import *
+
+class TestTarifa(unittest.TestCase):
+    pass
+
+class TestTiempoDeServicio(unittest.TestCase):
+    pass
+
+class TestCalcularPrecio(unittest.TestCase):
+    pass
