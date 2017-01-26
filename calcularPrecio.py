f0b9cc082bc1caadd0ee86a15b97ebca00165e19
Jose Acevedo <joseluisacevedo1995@gmail.com>
2017-01-26 11:26:08 -0400

    Agregar funciones para pruebas unitarias en PyUnit

--- calcularPrecio.py
+++ calcularPrecio.py
@@ -9,9 +9,35 @@ import datetime
 import decimal
 
 
+# Verificar tiempo de servicio valido
+def verificarServicio(inicio, final):
+    
+    try:
+        assert((final-inicio).total_seconds() >= 15*60)
+    except:
+        return("Duracion de servicio debe ser mayor o igual a 15 minutos")
+        sys.exit()
+
+    try:
+        assert((final-inicio).total_seconds() <= 7*24*3600)
+    except:
+        return("Duracion de servicio debe ser menor o igual a 7 dias")
+        sys.exit()
+
+
+# Verifica tarifa valida
+def verificarTarifa(semana, finDeSemana):
+    try:
+        assert(semana >= 0 and finDeSemana >= 0)
+    except:
+        return("Monto de tarifa no puede ser negativo")
+        sys.exit()
+
+
 # Objetos del tipo tarifa para dias durante semana y fines de semana.
 class Tarifa:
     def __init__(self, semana, finDeSemana):
+        verificarTarifa(semana, finDeSemana)
         self.semana = decimal.Decimal(semana)
         self.finDeSemana = decimal.Decimal(finDeSemana)
     
@@ -19,6 +45,7 @@ class Tarifa:
 # Objetos para el intervalo de trabajo
 class TiempoDeTrabajo:
     def __init__(self, inicioDeServicio, finDeServicio):
+        verificarServicio(inicioDeServicio, finDeServicio)
         self.inicioDeServicio = inicioDeServicio
         self.finDeServicio = finDeServicio
 
@@ -27,77 +54,33 @@ class TiempoDeTrabajo:
 def finDeSemana(dia):
     return  dia.weekday() == 5 or \
             dia.weekday() == 6
-
-
-# Verificar tiempo de servicio valido
-def verificarServicio(servicio):
-    
-    inicio = servicio.inicioDeServicio
-    final  = servicio.finDeServicio
-    
-    try:
-        assert((final-inicio).total_seconds() >= 15*60)
-    except:
-        print("Duracion de servicio debe ser mayor o igual a 15 minutos")
-        sys.exit()
-
-    try:
-        assert((final-inicio).total_seconds() <= 7*24*3600)
-    except:
-        print("Duracion de servicio debe ser menor o igual a 7 dias")
-        sys.exit()
-
-
-# Verifica tarifa valida
-def verificarTarifa(tarifa):
-    try:
-        assert(tarifa.semana >= 0 and tarifa.finDeSemana >= 0)
-    except:
-        print("Monto de tarifa no puede ser negativo")
-        sys.exit()
     
 
 # Calcula precio a pagar
+# 
+#    - Dominio de Datos:
+#    
+#        El dominio de la función consta de pares ordenados cuya 1ra coordenada es
+#        un objeto del tipo tarifa y la 2da coordenada es un objeto de tipo tiempoDeServicio.
+#
+#        La tarifa determinar el monto a pagar por hora en dias de semana y fines de semana.
+#        Los montos no pueden ser negativos.
+#
+#        El tiempoDeServicio almacena el tiempo transcurrido del trabajo. Dicho tiempo se encuentra
+#        entre 15 minutos y 7 dias.
+#
 def calcularPrecio(tarifa, tiempoDeServicio):
-
-    verificarServicio(tiempoDeServicio)
-    verificarTarifa(tarifa)
 
     precio = 0
     inicio = tiempoDeServicio.inicioDeServicio
     final  = tiempoDeServicio.finDeServicio
-
-    # Primer dia de trabajo
-    if finDeSemana(inicio):
-        precio += (24 - inicio.hour) * tarifa.finDeSemana
-    else:
-        precio += (24 - inicio.hour) * tarifa.semana
-
-    print(precio)
-
-    # Dias intermedios de trabajo
-    dia = (inicio.weekday() + 1) % 7
-    for i in range((final.date() - inicio.date()).days - 1):
-        if dia == 5 or dia == 6:
-            precio += 24*tarifa.finDeSemana
+    
+    dia = inicio
+    while dia < final:
+        if finDeSemana(dia):
+            precio += tarifa.finDeSemana
         else:
-            precio += 24*tarifa.semana
-
-        dia = (dia + 1) % 7
-
-    print(precio)
-
-    # Ultimo dia de trabajo
-    if(final > inicio):
-        if finDeSemana(final):
-            precio += final.hour * tarifa.finDeSemana
-            if(final.minute > 0):
-                precio += tarifa.finDeSemana
-        else:
-            precio += final.hour * tarifa.semana
-            if(final.minute > 0 or final.second > 0):
-                precio += tarifa.semana
-
-        print(precio)
+            precio += tarifa.semana
+        dia += datetime.timedelta(hours=1)
 
     return precio
--- pruebas.py
+++ pruebas.py
@@ -6,13 +6,165 @@
 #
 
 import unittest
+from datetime import *
+from decimal import Decimal
 from calcularPrecio import *
+from logging import exception
 
+# Pruebas para la clase de Tarifa
 class TestTarifa(unittest.TestCase):
-    pass
+    
+    def testTarifaNegativaSemana(self):
+        try:
+            t = Tarifa("-1","1")
+        except:
+            pass
+            
+    def testTarifaNegativaFinDeSemana(self):
+        try:
+            t = Tarifa("1","-1")
+        except:
+            pass
 
+    def testTarifaNula(self):
+        t = Tarifa("0","0")
+        self.assertEqual(t.semana,Decimal("0"))
+        self.assertEqual(t.finDeSemana,Decimal("0"))
+
+    def testTarifaValida(self):
+        t = Tarifa("10","20")
+        self.assertEqual(t.semana,Decimal("10"))
+        self.assertEqual(t.finDeSemana,Decimal("20"))
+
+
+# Pruebas para la clase de Tiempos de Servicio
 class TestTiempoDeServicio(unittest.TestCase):
-    pass
+    
+    def testTiempoDeServicioCorrecto(self):
+        t = TiempoDeTrabajo(datetime.datetime(2017,01,21,2,30),datetime.datetime(2017,01,28,10,15))
+        self.assertEqual(t.inicioDeServicio,datetime.datetime(2017,01,21,2,30))
+        self.assertEqual(t.finDeServicio,datetime.datetime(2017,01,28,10,15))
 
+    def testTiempoDeServicioNegativo(self):
+        # Servicio de 10min
+        try:
+            t = TiempoDeTrabajo(datetime.datetime(2017,02,30),datetime.datetime(2017,01,30,0,10))
+        except:
+            pass
+        else:
+            self.assertIsNotNone(t)
+    
+    def testTiempoDeServicioCorto(self):
+        # Servicio de 10min
+        try:
+            t = TiempoDeTrabajo(datetime.datetime(2017,01,30),datetime.datetime(2017,01,30,0,10))
+        except:
+            pass
+        else:
+            self.assertIsNotNone(t)
+
+    def testTiempoDeServicioMinimo(self):
+        # Servicio de 15min
+        try:
+            t = TiempoDeTrabajo(datetime.datetime(2017,01,30),datetime.datetime(2017,01,30,0,15))
+        except:
+            assert False
+        
+    def testTiempoDeServicioMaximo(self):
+        # Servicio de 7 dias
+        try:
+            t = TiempoDeTrabajo(datetime.datetime(2017,01,21),datetime.datetime(2017,01,28))
+        except:
+            assert False
+
+    def testTiempoDeServicioLargo(self):
+        # Servicio de 10 dias
+        try:
+            t = TiempoDeTrabajo(datetime.datetime(2017,01,20),datetime.datetime(2017,01,30))
+        except:
+            pass
+        else:
+            self.assertIsNotNone(t)
+            
+
+# Pruebas para la verificar correctitud de precios.
 class TestCalcularPrecio(unittest.TestCase):
-    pass
+
+    def testCosto15minSemana(self):
+        tarifa = Tarifa("15","2.8")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,20,0,15),datetime.datetime(2017,01,20,0,30))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),15)
+
+    def testCosto30minSemana(self):
+        tarifa = Tarifa("30","2")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,20,0),datetime.datetime(2017,01,20,0,30))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),30)
+    
+    def testCosto1horaSemana(self):
+        tarifa = Tarifa("1","2")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,20,0),datetime.datetime(2017,01,20,1))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),1)
+
+    def testCosto24horaSemana(self):
+        tarifa = Tarifa("1","0")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,20),datetime.datetime(2017,01,21))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),24)
+
+    def testCosto3diasSemana(self):
+        tarifa = Tarifa("3","0")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,17),datetime.datetime(2017,01,20))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),216)
+        
+    def testCosto15minFinDeSemana(self):
+        tarifa = Tarifa("1","2")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,28,0,15),datetime.datetime(2017,01,28,0,30))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),2)
+
+    def testCosto30minFinDeSemana(self):
+        tarifa = Tarifa("30","40")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,28,0),datetime.datetime(2017,01,28,0,30))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),40)
+    
+    def testCosto1horaFinDeSemana(self):
+        tarifa = Tarifa("1","2")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,28,14,30),datetime.datetime(2017,01,28,15,30))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),2)
+
+    def testCosto24horaFinDeSemana(self):
+        tarifa = Tarifa("1","10")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,28,13),datetime.datetime(2017,01,29,13))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),240)
+
+    def testCosto2diasFinDeSemana(self):
+        tarifa = Tarifa("1000","1")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,28),datetime.datetime(2017,01,30))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),48)
+
+    def testCostoCambiodeDia(self):
+        tarifa = Tarifa("1","0.5")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,18,23,30),datetime.datetime(2017,01,19,1,30))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),2)
+
+    def testCostoSemanayLuegoFin(self):
+        tarifa = Tarifa("1","10")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,20,23),datetime.datetime(2017,01,21,1))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),11)
+
+    def testCostoFinyLuegoSemana(self):
+        tarifa = Tarifa("1","10")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,22,23),datetime.datetime(2017,01,23,1))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),11)
+
+    def testCosto7dias(self):
+        tarifa = Tarifa("1","10")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,16),datetime.datetime(2017,01,23))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),600)
+
+    def testCobrarHoraCompletaPorMinutosSobrantes(self):
+        tarifa = Tarifa("1","10")
+        tiempo = TiempoDeTrabajo(datetime.datetime(2017,01,20,10),datetime.datetime(2017,01,20,11,1))
+        self.assertEqual(calcularPrecio(tarifa,tiempo),2)
+        
+
+if __name__ == "__main__":
+    unittest.main()
